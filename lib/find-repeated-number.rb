e038b50f7696c5007c51f3e28cbb352ff7b476fc
Schyler Hosler <schylerhosler@gmail.com>
2016-10-24 17:11:54 -0400

    Ruby practice with finding dupes in array of numbers all tests passing

--- lib/find-repeated-number.rb
+++ lib/find-repeated-number.rb
@@ -1,3 +1,31 @@
-def find_repeated_number(array)
+require 'pry'
 
+# def find_repeated_number(array)
+#   counter = 1
+#   orange = array[counter]
+#   array.each do |apple|
+#     binding.pry
+#     if apple == orange
+#       return orange
+#     else
+#       orange = array[counter += 1]
+#     end
+#   end
+# end
+
+# above only works for consecutive dupes
+
+def find_repeated_number(array)
+  counter = 0
+  until counter > array.length do
+    apple = array[counter]
+    array.each_with_index do |orange, index|
+      if index > counter
+        if apple == orange
+          return orange
+        end
+      end
+    end
+    counter += 1
+  end
 end
